24b47187e6e8ba3c77bd59c97bf7a7b9ba91e5da
Ibrahim <33922797+SleepySwords@users.noreply.github.com>
2026-07-07 18:02:20 +1000

    feat: done but slow :(

--- bin/main.ml
+++ bin/main.ml
@@ -6,43 +6,77 @@ let ( +.. ) = Vec3.( +.. )
 let ( -.. ) = Vec3.( -.. )
 let ( *.. ) = Vec3.( *.. )
 let ( /.. ) = Vec3.( /.. )
-let material_ground = lambertian_scatter ~albedo:(Vec3.make 0.8 0.8 0.0)
-let material_centre = lambertian_scatter ~albedo:(Vec3.make 0. 0.2 0.5)
-let material_left = dialetic_scatter ~refraction_index:1.5
-let material_bubble = dialetic_scatter ~refraction_index:(1. /. 1.5)
-let material_right = metal_scatter ~albedo:(Vec3.make 0.8 0.6 0.2) ~fuzz:1.0
+
+let material_ground = lambertian_scatter ~albedo:(Vec3.make 0.5 0.5 0.5)
+
+let material1 = dialetic_scatter ~refraction_index:1.5
+let material2 = lambertian_scatter ~albedo:(Vec3.make 0.4 0.2 0.1)
+let material3 = metal_scatter ~albedo:(Vec3.make 0.7 0.6 0.5) ~fuzz:0.
 
 let world =
   [
     mkSphereGen
       {
-        centre = Vec3.make 0.0 (-100.5) (-1.0);
-        radius = 100.0;
+        centre = Vec3.make 0.0 (-1000.) 0.0;
+        radius = 1000.0;
         mat = material_ground;
       };
     mkSphereGen
-      { centre = Vec3.make 0.0 0.0 (-1.2); radius = 0.5; mat = material_centre };
-    mkSphereGen
       {
-        centre = Vec3.make (-1.0) 0.0 (-1.0);
-        radius = 0.5;
-        mat = material_left;
+        centre = Vec3.make 0.0 1. 0.0;
+        radius = 1.0;
+        mat = material1;
       };
     mkSphereGen
       {
-        centre = Vec3.make (-1.0) 0.0 (-1.0);
-        radius = 0.4;
-        mat = material_bubble;
+        centre = Vec3.make (-.4.) 1. 0.;
+        radius = 1.0;
+        mat = material2;
       };
     mkSphereGen
-      { centre = Vec3.make 1.0 0.0 (-1.0); radius = 0.5; mat = material_right };
+      {
+        centre = Vec3.make 4. 1. 0.;
+        radius = 1.0;
+        mat = material3;
+      };
   ]
 
+let world =
+  world
+  @ List.init (22 * 22) (fun i ->
+      let a = (i / 22) - 11 in
+      let b = (i mod 22) - 11 in
+      let choose_chance = Vec3.random_float_range 0. 1. in
+      let mat =
+        match choose_chance with
+        | x when x < 0.8 ->
+            lambertian_scatter
+              ~albedo:
+                (Vec3.multiply_vec
+                   (Vec3.random_vec_range 0. 1.)
+                   (Vec3.random_vec_range 0. 1.))
+        | x when x < 0.95 ->
+            metal_scatter
+              ~albedo:(Vec3.random_vec_range 0.5 1.)
+              ~fuzz:(Vec3.random_float_range 0. 0.5)
+        | otherwise -> dialetic_scatter ~refraction_index:1.5
+      in
+      mkSphereGen
+        {
+          centre =
+            Vec3.make
+              (float_of_int a +. Vec3.random_float_range (-0.9) 0.9)
+              0.2
+              (float_of_int b +. Vec3.random_float_range (-0.9) 0.9);
+          radius = 0.2;
+          mat;
+        })
+
 let aspect_ratio = 16.0 /. 9.0
-let image_width = 400
+let image_width = 600
 
 let camera =
-  Camera.make ~samples_per_pixel:100 ~vfov:20.
+  Camera.make ~samples_per_pixel:10 ~vfov:20. ~max_depth:10
     ~look_from:(Vec3.make (-2.) 2. 1.) ~look_at:(Vec3.make 0. 0. (-1.))
     ~defocus_angle:10. ~focus_dist:3.4 image_width aspect_ratio
 
